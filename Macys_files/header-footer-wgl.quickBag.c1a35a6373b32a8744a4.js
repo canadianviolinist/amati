1ca0361d3800a97f0731b0ea05c31c5c9af10062
shekarpathi <tambaram@gmail.com>
2022-10-11 07:53:39 -0400

    Changes

--- Macys_files/header-footer-wgl.quickBag.c1a35a6373b32a8744a4.js
+++
@@ -1,2 +0,0 @@
-header_footer_wgl_jsonp([36],{738:function(module,exports){}});
-//# sourceMappingURL=header-footer-wgl.quickBag.c1a35a6373b32a8744a4.js.map
--- Macys_files/ordermodsreturns-ui.0.714f051f3ea0a1b75ad1.js
+++
@@ -1,2 +0,0 @@
-(window.ordermodsreturns_ui_jsonp=window.ordermodsreturns_ui_jsonp||[]).push([[0],{cabea379:function(o,n,r){}}]);
-//# sourceMappingURL=ordermodsreturns-ui.0.714f051f3ea0a1b75ad1.js.map
